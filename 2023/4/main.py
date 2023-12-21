f94a6edca4364df6b5a02eb4f03ebd275d399391
stormhurricane <s.zmiewski@gmx.de>
2023-12-22 00:24:25 +0100

    changed place of for loop, so much better
    Day 4

--- 2023/4/main.py
+++ 2023/4/main.py
@@ -9,13 +9,12 @@ def main(filename: str):
 
     score = 0
     for i, card in enumerate(scratchcards):
+        winning_numbers, owned_numbers = card.split(":")[1].split("|")
+        winning_numbers = [number for number in winning_numbers.split(" ") if number.isdigit()]
+        owned_numbers = [number for number in owned_numbers.split(" ") if number.isdigit()]
+        matches = sum(1 for number in owned_numbers if number in winning_numbers)
+        worth = int(2 ** (matches - 1))
         for r in range(copies[i]):
-            winning_numbers, owned_numbers = card.split(":")[1].split("|")
-            winning_numbers = [number for number in winning_numbers.split(" ") if number.isdigit()]
-            owned_numbers = [number for number in owned_numbers.split(" ") if number.isdigit()]
-            matches = sum(1 for number in owned_numbers if number in winning_numbers)
-            worth = int(2 ** (matches - 1))
-           # print(f"Card {i + 1} matches {matches}")
             for x in range(i+1, i+1+matches):
                 copies[x] = copies[x] + 1
         score += worth
