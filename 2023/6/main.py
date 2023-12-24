e80b15d3aac73dcb91223693bfe629d18c3530c6
ist1108980 <sascha.zmiewski@tecnico.ulisboa.pt>
2023-12-24 19:07:56 +0100

    day 6 part 2

--- 2023/6/main.py
+++ 2023/6/main.py
@@ -1,5 +1,17 @@
 import re
-import math
+
+
+def find_number_of_solutions(time, distance):
+    high, low = 0, 0
+    for i in range(1, time - 1):
+        if i * (time - i) > distance:
+            low = i
+            break
+    for i in range(time - 1, low, -1):
+        if i * (time - i) > distance:
+            high = i
+            break
+    return high - low + 1
 
 
 def main(filename):
@@ -15,23 +27,19 @@ def main(filename):
     for race in races:
         (time, distance) = race
         print(f"Race: {time} with {distance}")
-        high, low = 0, 0
-        for i in range(1, time - 1):
-            if i * (time - i) > distance:
-                low = i
-                break
-        for i in range(time - 1, low, -1):
-            if i * (time - i) > distance:
-                high = i
-                break
 
-        possible_solutions.append(high - low + 1)
+        possible_solutions.append(find_number_of_solutions(time, distance))
+
     prod = 1
     for val in possible_solutions:
         prod = prod * val
 
+    time = int(lines[0].split(":")[1].replace(" ", ""))
+    distance = int(lines[1].split(":")[1].replace(" ", ""))
+
     print(f"Product of number of possible solutions: {prod}")
-    print(possible_solutions)
+    print(
+        f"Solutions for big race: {find_number_of_solutions(time, distance)}")
 
 
 if __name__ == "__main__":
