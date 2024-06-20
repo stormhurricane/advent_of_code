0f209ca6ae15297cdc8ae18e44e6f8df539b83f4
sascha <s.zmiewski@gmx.de>
2024-06-21 01:01:50 +0200

    3/2015

--- 2015/day_3/main.py
+++ 2015/day_3/main.py
@@ -10,13 +10,32 @@ if __name__ == "__main__":
         "<": (0, -1)
     }
 
-    coordinates = [(0,0)]
+    coordinates = {
+        "santa": [(0,0)],
+        "robo-santa": [(0, 0)],
+        "all": [(0, 0)]
+        }
+    
 
-    for char in input:
-        new_coordinates = tuple(map(lambda x, y: x - y, coordinates[-1], location[char]))
-        coordinates.append(new_coordinates)
+    for i, char in enumerate(input):
+        if i % 2 == 0:
+            key = "santa"
+        else:
+            key = "robo-santa"
 
-    unique_coordinates = set(coordinates)
-    print(f"Santa visits {len(unique_coordinates)} unique houses!")
+        new_coordinates = tuple(map(lambda x, y: x - y, coordinates[key][-1], location[char]))
+        coordinates[key].append(new_coordinates)
+
+        new_coordinates = tuple(map(lambda x, y: x - y, coordinates["all"][-1], location[char]))
+        coordinates["all"].append(new_coordinates)
+        
+
+    unique_coordinates_of_santa = set(coordinates["all"])
+
+    combined_list = coordinates["santa" ] + coordinates["robo-santa"]
+    unique_coordinates_of_both = set(combined_list)
+
+    print(f"Santa visited {len(unique_coordinates_of_santa)} unique houses in the previous year!")
+    print(f"Santa and Robo-Santa visited {len(unique_coordinates_of_both)} unique houses!")
 
     
